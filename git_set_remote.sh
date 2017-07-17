9b290ae8a3df3683f1758aba1c943c35c112126d
David Robinson <david.pl.robinson@googlemail.com>
2017-07-17 19:16:03 +1000

    initial

---
+++ git_set_remote.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "David Robinson"
+git config --global user.email "david.pl.robinson@googlemail.com"
+git config credential.helper store
+git remote add gitlab http://github.com/davidplrobinson/gitlab.git
